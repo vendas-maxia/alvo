36f5245c6b6587911629280df18500d8358dcdd6
vendas-maxia <vendas@maxiagestao.com>
2026-08-13 21:03:49 -0300

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import BaseClientesMaxfio from "CRMSicrediHauer.jsx";
+import CRMSicrediHauer from "./CRMSicrediHauer.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BaseClientesMaxfio />
+    <CRMSicrediHauer />
   </React.StrictMode>
 );
