80d6242d3318243db14c1829c5606d9db76ba378
vendas-maxia <vendas@maxiagestao.com>
2026-08-13 21:01:24 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import BaseClientesMaxfio from "./BaseClientesMaxfio.jsx";
+import BaseClientesMaxfio from "CRMSicrediHauer.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
